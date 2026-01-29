4a2eb6b08a6fa756c27cb3cb5c2eea23d8a35bf0
JohnCornflakes <johncornflakes@yahoo.com>
2026-01-28 18:46:23 -0800

    Creates and completes version for 1.21.2 NeoForge

--- src/main/java/me/JohnCornflakes/growthhormonenf/item/ModItems.java
+++ src/main/java/me/JohnCornflakes/growthhormonenf/item/ModItems.java
@@ -1,6 +1,9 @@
 package me.JohnCornflakes.growthhormonenf.item;
 
 import me.JohnCornflakes.growthhormonenf.Growthhormonenf;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -10,7 +13,8 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Growthhormonenf.MODID);
 
     public static final DeferredItem<Item> GROWTH_HORMONE = ITEMS.register("growth_hormone",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().setId(ResourceKey.create(Registries.ITEM,
+                    ResourceLocation.fromNamespaceAndPath(Growthhormonenf.MODID, "growth_hormone")))));
 
 
 
